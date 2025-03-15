0e6cc783cbb2457a80ec146a5018718adc510481
AlexDvor <alexmatvichuk@gmail.com>
2025-03-15 17:07:34 +0100

    Ref

--- Task_1/task.py
+++ Task_1/task.py
@@ -5,8 +5,6 @@ def total_salary(path:str):
 	relative_path= Path(path)
 	absolute_path = relative_path.absolute()
 	some_num =[]
-	total = None
-	average= None
 
 	try:
 		with open(absolute_path,"r" ,encoding='utf-8') as file:
@@ -18,7 +16,8 @@ def total_salary(path:str):
 		
 		total = sum(some_num)
 		average = total // len(items)
-		return f"Загальна сумма заробітньої плати:{total}, Середня заробітня платня:{average} "
+		
+		return total, average
 	
 	except FileNotFoundError:
 		print("File is not exist,try to use other path")
