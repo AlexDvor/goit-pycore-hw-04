bc68c102cf069ab3513e467e069b1ca2a7b17f3d
AlexDvor <alexmatvichuk@gmail.com>
2025-03-17 10:43:55 +0100

    ref

--- Task_4/main.py
+++ Task_4/main.py
@@ -1,30 +1,67 @@
 import sys
 
+contacts = []
+
+
+def add_contact(name: str, phone: str):
+    contacts.append({"name": name, "phone": phone})
+    print("Contact added!")
+    print(contacts)
+
+
+def change_contact(name: str, phone: str):
+    pass
+
+
+def show_phone(name: str):
+    for contact in contacts:
+        if contact["name"] == name:
+            print(contact["phone"])
+            return
+    print("Contact not found!")
+
 
 def parse_input(inputted_data: list[str]):
-    pass
+    command = inputted_data[1:]
+    return command
 
 
-def handler_input():
-    # add_contact(), change_contact(), show_phone()
-    pass
+def handler_input(command_list: list[str]):
+    match command_list:
+        case ["add", name, phone]:
+            add_contact(name, phone)
+        case ["change", name, phone]:
+            change_contact(name, phone)
+        case ["show", name]:
+            show_phone(name)
+        case ["exit"] | ["close"]:
+            print("Goodbye!")
+            return
+        case _:
+            print("Invalid command!")
 
 
 def main():
     print("Welcome to the assistant bot!")
-
+    command = parse_input(sys.argv)
     while True:
-        inputted_data = input("Enter command: ").split()
-        if inputted_data[0] == "exit":
+        if command in ["exit", "close"]:
             print("Goodbye!")
             break
-        parse_input(inputted_data)
-        handler_input()
+        elif command == "hello":
+            print("How can I help you?")
+
+        elif len(command) == 3:
+            handler_input(command)
+
+        else:
+            print("Invalid command!")
+            break
+
+
+# python main.py show Oleksii
+# python main.py add Oleksii 459621475
 
 
 if __name__ == "__main__":
     main()
-
-
-# python main.py
-# python main.py add Oleksii 459621475
